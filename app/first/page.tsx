8d143b7cfaf08e6ef5950be6b16c0c5ab1864122
Dmitrii Bachin <ska18@mail.ru>
2024-04-19 12:51:47 +0300

    first page

---
+++ app/first/page.tsx
@@ -0,0 +1,14 @@
+import './style.css'
+export default function page() {
+  return (
+    <section>
+      <div className='box-1'>
+        <div className='box-1-1'></div>
+      </div>
+      <br /> <br />
+      <div className='box-2'>
+        <div className='box-2-1'></div>
+      </div>
+    </section>
+  )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,16 @@
+import Link from 'next/link'
+
 export default function Home() {
   return (
     <section className='py-24'>
       <div className='container'>
-        <div className='text-3xl font-bold'>Next TS Starter</div>
+        <div className='mb-2 text-3xl font-bold'>Приветствую на страничке</div>
+        <div className='mb-5'>
+          <p>тут будут выкладываться мой прогресс в html/css и возможно js</p>
+        </div>
+        <div>
+          <Link href='/first'> - Первая</Link>
+        </div>
       </div>
     </section>
   )
